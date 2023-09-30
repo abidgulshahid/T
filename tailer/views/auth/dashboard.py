592562c2611611cbe74d9d7dc8faa5dc00d3d564
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-10-01 04:16:55 +0500

    Update dashboard.py

--- tailer/views/auth/dashboard.py
+++ tailer/views/auth/dashboard.py
@@ -21,6 +21,13 @@ class Dashboard(View):
                 context = {'get_tailers': get_tailers}
                 string = render(request, 'tailer/_partial/_list.html', context=context)
                 return HttpResponse(string)
+            if 'obj' in request.GET:
+                get_obj = request.GET.get('obj')
+                obj = Tailor.objects.filter(id=get_obj, user_id=request.user.id).first()
+                edit_form  =TailorForm(instance=obj)
+                context =  {'entry_form': edit_form}
+                string = render(request, 'tailer/_partial/_edit.html', context=context)
+                return HttpResponse(string)
             return render(request, 'tailer/dashboard.html', context={'request':request, 'entry_form':form})
         return HttpResponseRedirect(reverse_lazy('index_view'))
 
@@ -29,12 +36,19 @@ class Dashboard(View):
 
         if 'create_entry' in request.POST:
             form = TailorForm(data=request.POST)
-            print(request.user.id)
             if form.is_valid():
-                print('hello')
-                form = form.save(commit=False)
-                print(form, 'form')
-                form.save()
+                data = form.cleaned_data
+                obj = form.save(commit=False)
+                obj.user = request.user
+                obj.save()
                 return HttpResponse('success')
             print(form.errors)
             return render(request, 'tailer/_partial/_create.html', context={'request': request, 'entry_form': form})
+
+        if 'edit_entry' in request.POST:
+            edit_entry = request.POST.get('edit_entry')
+            edit_form = TailorForm(instance=Tailor.objects.filter(user_id=request.user.id, id=edit_entry))
+            if edit_form.is_valid():
+                edit_form.save()
+                return HttpResponse("success")
+            return render(request, 'tailer/_partial/_edit.html', context={'request': request, 'entry_form': edit_form})
