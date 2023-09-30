82e06a0163c656fc4828e8829fcb5cc533265ec7
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-09-30 23:17:49 +0500

    code

--- tailer/models.py
+++ tailer/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Tailor(models.Model):
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
+    name = models.CharField(max_length=500, null=True, blank=True)
+    height = models.CharField(max_length=500, null=True, blank=True)
+    phone_number = models.CharField(max_length=500, null=True, blank=True)
+    chest = models.CharField(max_length=500, null=True, blank=True)
+    right_shoulder = models.CharField(max_length=500, null=True, blank=True)
+    left_shoulder = models.CharField(max_length=500, null=True, blank=True)
+    type = models.CharField(max_length=500, null=True, blank=True)
+    upper_shirt = models.CharField(max_length=500, null=True, blank=True)
+    trouser = models.CharField(max_length=500, null=True, blank=True)
+
+
+
