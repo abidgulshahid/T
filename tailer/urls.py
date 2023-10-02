5d04b4db9526cb3020b7568ef7b1b36ea93f5c05
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-10-02 22:40:26 +0500

    Update urls.py

--- tailer/urls.py
+++ tailer/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from tailer.views.auth import login, register, dashboard
+from tailer.views.auth.dashboard import logout
 
 urlpatterns = [
     path('', login.LoginView.as_view(), name='index_view'),
     path('register/', register.RegisterView.as_view(), name='signup_view'),
-    path('dasboard/',dashboard.Dashboard.as_view(), name='dashboard_view')
+    path('dasboard/',dashboard.Dashboard.as_view(), name='dashboard_view'),
+    path('logout/',dashboard.LogoutView.as_view(), name='logout_view'),
 ]
