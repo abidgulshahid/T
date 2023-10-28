426ddde0776c154033cf1b59ffd56f98f3d9c5bc
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-10-28 13:17:27 +0500

    required field

--- tailer/forms/tailor_form.py
+++ tailer/forms/tailor_form.py
@@ -3,17 +3,16 @@ from django import forms
 from tailer.models import Customer
 
 
-
 class TailorForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = '__all__'
 
     def __init__(self, *args, **kwargs):
-        super(TailorForm, self).__init__(*args,  **kwargs)
+        super(TailorForm, self).__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = "form-control"
-            # self.fields[field].required = True
+            self.fields[field].required = False
         self.fields['order_take'].widget = forms.TextInput(attrs={'type': 'date'})
         self.fields['order_take'].widget.attrs['class'] = "form-control"
 
