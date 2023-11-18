890825a69ce3aac9ffabc1160bac2b11bf8e60c1
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-11-18 13:25:11 +0500

    Update admin.py

--- tailer/admin.py
+++ tailer/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Customer, Users
+from .models import Customer, Users, RequestAccess
 
 # Register your models here.
 
 admin.site.register(Customer)
 admin.site.register(Users)
+admin.site.register(RequestAccess)
