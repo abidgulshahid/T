fed67adc9d57e9bf132a469a041a71887b182a5b
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-11-19 17:49:43 +0500

    lgo

--- tailer/views/auth/login.py
+++ tailer/views/auth/login.py
@@ -7,21 +7,27 @@ from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt
 from tailer.forms.auth import LoginForm
 from django.utils.decorators import method_decorator
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+
 
 class LoginView(View):
     @method_decorator(csrf_exempt)
-
     def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user.is_tailer:
+            return HttpResponseRedirect(reverse_lazy('dashboard_view'))
+        elif request.user.is_authenticated and request.user.is_superuser:
+            admin_url = reverse('admin:index')
+            return redirect(admin_url)
         return super(LoginView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request):
-        # if request.user.is_authenticated:
-        #     return HttpResponseRedirect(reverse_lazy('home_view'))
         form = LoginForm()
         context = {'form': form}
         return render(request, 'tailer/login.html', context=context)
 
-    def post(self,request):
+    def post(self, request):
         print('reqest', 'here')
         form = LoginForm(data=request.POST)
         if form.is_valid():
@@ -31,7 +37,7 @@ class LoginView(View):
             auth = authenticate(**data)
             print(auth, 'auth')
             if auth:
-                login(request,auth)
+                login(request, auth)
                 return HttpResponseRedirect(reverse_lazy('dashboard_view'))
             return HttpResponseRedirect(reverse_lazy('index_view'))
         print(form.errors)
--- tailer/views/auth/register.py
+++ tailer/views/auth/register.py
@@ -1,9 +1,8 @@
 import json
-from audioop import reverse
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render,redirect
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views import View
 from django.contrib.auth import authenticate, login
 
@@ -13,6 +12,11 @@ from tailer.models import Users
 
 class RegisterView(View):
     def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user.is_tailer:
+            return HttpResponseRedirect(reverse_lazy('dashboard_view'))
+        elif request.user.is_authenticated and request.user.is_superuser:
+            admin_url = reverse('admin:index')
+            return redirect(admin_url)
         return super(RegisterView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request):
